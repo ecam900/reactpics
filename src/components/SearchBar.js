6e11964917c4b1dc1a6e63f42cab8f7c3746a642
Eric Crescioni <ericcrescioni@reliabletradinginc.com>
2019-02-01 17:44:34 -0400

    Add state to SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: 'Search here...' };
+
+    // This is to prevent the browser from refreshing the page whenever user presses ENTER key.
+    onFormSubmit(event) {
+        event.preventDefault();
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input 
+                            type="text" 
+                            value={this.state.term} 
+                            onChange={(e) => this.setState({ term: e.target.value }) 
+                        } />
                     </div>
                 </form>
             </div>
