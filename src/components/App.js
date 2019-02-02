7b3bd9f1d15918cea2f50b62ab0a364c9102d980
Eric Crescioni <ecam900@gmail.com>
2019-02-01 22:07:44 -0400

    Add axios. Connect axios to unsplash api. Add API ClientID.

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
+
 
 class App extends React.Component {
   onSearchSubmit(term) {
-    console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params:{
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID 55159597b7e9752aec40d67a4b1b3cf0c51f4b18d4e60988b9089700304539f8'
+      }
+    });
   }
 
 
